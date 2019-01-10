6d5d4b9d53ff0bd26bc14de3c3946a6f21353c51
Yamagn <syankenpon@gmail.com>
2019-01-10 15:07:33 +0900

    Initial Commit

---
+++ hanasaka_times/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hanasaka_times
+//
+//  Created by ymgn on 2019/01/10.
+//  Copyright © 2019 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
